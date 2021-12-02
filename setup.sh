8bb817930a82396dfd006abaed100821aeb006ee
T2rrek <74355625+T0umm@users.noreply.github.com>
2021-12-02 17:33:09 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ pacman -Syy
 pacman -S flameshot terminator gimp krita
 wget https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
 pacman -U https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
+wget https://arch.alerque.com/x86_64/font-manager-0.8.7-2-x86_64.pkg.tar.zst
+pacman -U font-manager-0.8.7-2-x86_64.pkg.tar.zst
