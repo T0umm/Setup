224bbbac5fb223375f41314ecf3cd9ae6c9c8dbd
T2rrek <74355625+T0umm@users.noreply.github.com>
2021-12-02 17:11:54 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/
 passwd && passwd liveuser
 pacman -Syy
-pacman -S flameshot terminator
+pacman -S flameshot terminator gimp krita
 wget https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
 pacman -U https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
