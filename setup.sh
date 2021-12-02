ea94d80de479d30117047a3b8bd637a2f305d62d
T2rrek <74355625+T0umm@users.noreply.github.com>
2021-12-02 16:41:51 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/
+passwd && passwd liveuser
+pacman -Syy
+pacman -S flameshot terminator
+wget https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
+pacman -U https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
