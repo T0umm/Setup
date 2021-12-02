2fdf6eb1b0f46f64a45617bbf5d751024de50d77
T2rrek <74355625+T0umm@users.noreply.github.com>
2021-12-02 16:39:55 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-import pyfiglet
-print(pyfiglet.figlet_format("iTs WORK"))
+#!/usr/bin/
+passwd && passwd liveuser
+pacman -Syy
+pacman -S flameshot terminator
+wget https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
+pacman -U https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
