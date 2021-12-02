30f1dfd25c3734ac8d84826ac125baa025bbfef0
T2rrek <74355625+T0umm@users.noreply.github.com>
2021-12-02 17:38:13 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ wget https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar
 pacman -U https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
 wget https://arch.alerque.com/x86_64/font-manager-0.8.7-2-x86_64.pkg.tar.zst
 pacman -U font-manager-0.8.7-2-x86_64.pkg.tar.zst
+font-manager -i UbuntuMono-Bold.ttf UbuntuMono-BoldItalic.ttf UbuntuMono-Italic.ttf UbuntuMono-Regular.ttf
