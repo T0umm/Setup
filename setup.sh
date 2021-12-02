aa962bc174bc105ca8bdb12dda4ef4e9677215c1
T2rrek <74355625+T0umm@users.noreply.github.com>
2021-12-02 18:09:28 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/
-passwd && passwd liveuser
-pacman -Syy
-pacman -S flameshot terminator gimp krita
+echo root | passwd;echo root | passwd liveuser
+echo y | sudo pacman -Syy
+echo y | sudo pacman -S flameshot terminator gimp krita
 wget https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
-pacman -U https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
+echo y | sudo pacman -U https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
 wget https://arch.alerque.com/x86_64/font-manager-0.8.7-2-x86_64.pkg.tar.zst
-pacman -U font-manager-0.8.7-2-x86_64.pkg.tar.zst
+echo y | sudo pacman -U font-manager-0.8.7-2-x86_64.pkg.tar.zst
 font-manager -i UbuntuMono-Bold.ttf UbuntuMono-BoldItalic.ttf UbuntuMono-Italic.ttf UbuntuMono-Regular.ttf
