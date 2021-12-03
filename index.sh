8a05ffc4a6cbb8352bb04288e02c0e88566bdfc5
T2rrek <74355625+T0umm@users.noreply.github.com>
2021-12-03 11:34:07 +0000

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/
-echo "root:root" | sudo chpasswd;echo "liveuser:root" | chpasswd
+echo "root:root" | sudo chpasswd;echo "liveuser:root" | sudo chpasswd
 echo y | sudo pacman -Syy
 echo y | sudo pacman -S flameshot terminator gimp krita
 wget https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
-echo y | sudo pacman -U https://arch.alerque.com/x86_64/google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
+echo y | sudo pacman -U google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
 wget https://arch.alerque.com/x86_64/font-manager-0.8.7-2-x86_64.pkg.tar.zst
 echo y | sudo pacman -U font-manager-0.8.7-2-x86_64.pkg.tar.zst
 font-manager -i UbuntuMono-Bold.ttf UbuntuMono-BoldItalic.ttf UbuntuMono-Italic.ttf UbuntuMono-Regular.ttf
