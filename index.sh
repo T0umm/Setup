db0ab425e4ccc453e0b9eac7c771b937506c51b0
T2rrek <74355625+T0umm@users.noreply.github.com>
2021-12-03 11:52:44 +0000

    Update index.sh

--- index.sh
+++ index.sh
@@ -7,4 +7,4 @@ echo y | sudo pacman -U google-chrome-94.0.4606.71-1-x86_64.pkg.tar.zst
 wget https://arch.alerque.com/x86_64/font-manager-0.8.7-2-x86_64.pkg.tar.zst
 echo y | sudo pacman -U font-manager-0.8.7-2-x86_64.pkg.tar.zst
 wget https://github.com/T0umm/Setup/blob/main/UbuntuMono-Bold.ttf https://github.com/T0umm/Setup/blob/main/UbuntuMono-BoldItalic.ttf https://github.com/T0umm/Setup/blob/main/UbuntuMono-Italic.ttf https://github.com/T0umm/Setup/blob/main/UbuntuMono-Regular.ttf
-font-manager -i UbuntuMono-Bold.ttf UbuntuMono-BoldItalic.ttf UbuntuMono-Italic.ttf UbuntuMono-Regular.ttf
+font-manager -i Ubuntu_Mono/
