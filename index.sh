564220eee80ebb4afa0cef9113b2d484ac6fa43b
T2rrek <74355625+T0umm@users.noreply.github.com>
2021-12-12 07:50:56 +0000

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/
 echo "root:root" | sudo chpasswd;echo "liveuser:root" | sudo chpasswd
 echo y | sudo pacman -Syy
-echo y | sudo pacman -S flameshot terminator gimp krita wget 
+echo y | sudo pacman -S flameshot terminator gimp krita wget python-pip
 wget https://arch.alerque.com/x86_64/brave-1.29.79-1-x86_64.pkg.tar.zst
 echo y | sudo pacman -U brave-1.29.79-1-x86_64.pkg.tar.zst
 wget https://arch.alerque.com/x86_64/font-manager-0.8.7-2-x86_64.pkg.tar.zst
