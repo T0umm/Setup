dbfe4c7a3fb67fe1dc4d55c21586c2073df53c32
T2rrek <74355625+T0umm@users.noreply.github.com>
2021-12-21 18:50:54 +0000

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -6,7 +6,7 @@ wget https://arch.alerque.com/x86_64/brave-1.29.79-1-x86_64.pkg.tar.zst
 echo y | sudo pacman -U brave-1.29.79-1-x86_64.pkg.tar.zst
 wget https://arch.alerque.com/x86_64/font-manager-0.8.7-2-x86_64.pkg.tar.zst
 echo y | sudo pacman -U font-manager-0.8.7-2-x86_64.pkg.tar.zst
-wget https://arch.alerque.com/x86_64/yay-11.0.1-1-x86_64.pkg.tar.zst
-echo y | sudo pacman -U yay-11.0.1-1-x86_64.pkg.tar.zst
-wget https://aur.andontie.net/x86_64/visual-studio-code-bin-1.62.3-1-x86_64.pkg.tar.zst
-echo y | sudo pacman -U visual-studio-code-bin-1.62.3-1-x86_64.pkg.tar.zst
+wget https://arch.alerque.com/x86_64/yay-11.0.2-1-x86_64.pkg.tar.zst
+echo y | sudo pacman -U yay-11.0.2-1-x86_64.pkg.tar.zst
+wget https://aur.andontie.net/x86_64/visual-studio-code-bin-1.63.2-1-x86_64.pkg.tar.zst
+echo y | sudo pacman -U visual-studio-code-bin-1.63.2-1-x86_64.pkg.tar.zst
