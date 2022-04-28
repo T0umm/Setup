4bfb0a099a0b7f0434dae61b3888c03752d48f68
T2rrek <74355625+T0umm@users.noreply.github.com>
2022-04-28 21:14:39 +0000

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/
-echo "root:root" | sudo chpasswd;echo "liveuser:root" | sudo chpasswd
+echo "root:root" | sudo chpasswd;echo "manjaro:root" | sudo chpasswd
 echo y | sudo pacman -Syy
-echo y | sudo pacman -S flameshot terminator gimp krita wget python-pip
+echo y | sudo pacman -S flameshot wget vokoscreen gnome-2048 pyzo
 wget https://arch.alerque.com/x86_64/brave-1.29.79-1-x86_64.pkg.tar.zst
 echo y | sudo pacman -U brave-1.29.79-1-x86_64.pkg.tar.zst
-wget https://arch.alerque.com/x86_64/font-manager-0.8.7-2-x86_64.pkg.tar.zst
-echo y | sudo pacman -U font-manager-0.8.7-2-x86_64.pkg.tar.zst
-wget https://arch.alerque.com/x86_64/yay-11.0.2-1-x86_64.pkg.tar.zst
-echo y | sudo pacman -U yay-11.0.2-1-x86_64.pkg.tar.zst
-wget https://aur.andontie.net/x86_64/visual-studio-code-bin-1.63.2-1-x86_64.pkg.tar.zst
-echo y | sudo pacman -U visual-studio-code-bin-1.63.2-1-x86_64.pkg.tar.zst
