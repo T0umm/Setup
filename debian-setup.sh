7c9eb858845ad96c7754e475018738cd314c9b2d
T2rrek <74355625+T0umm@users.noreply.github.com>
2022-06-18 22:05:57 +0000

    Update debian-setup.sh

--- debian-setup.sh
+++ debian-setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/
-echo "root:root" | sudo chpasswd;echo "liveuser:root" | sudo chpasswd
+echo "root:root" | sudo chpasswd;echo "zorin:root" | sudo chpasswd
 echo Yes | sudo apt-get update
-echo Yes | sudo apt-get install flameshot terminator gimp krita wget pip alacarte gdebi chromium font-manager 
-wget https://az764295.vo.msecnd.net/stable/7db1a2b88f7557e0a43fec75b6ba7e50b3e9f77e/code_1.63.0-1638855526_amd64.deb
-echo Yes | gdebi code_1.63.0-1638855526_amd64.deb
+echo Yes | sudo apt-get install flameshot gdebi gnome-tweaks
+sudo snap install chromium
